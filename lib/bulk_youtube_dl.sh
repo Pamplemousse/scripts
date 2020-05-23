3f1f12d150229b277fc51944004f7814c96870c4
Pamplemousse <xav.maso@gmail.com>
2020-05-24 00:28:00 +0200

    Correct tests and script for `bytdl`
      * `echo` works
      * clearer test output (STDOUT not polluted by the commands)
      * make it work...

--- lib/bulk_youtube_dl.sh
+++ lib/bulk_youtube_dl.sh
@@ -1,27 +1,21 @@
-#!/bin/zsh
+#!/usr/env/bin zsh
 # Usage: bytdl SOURCES DESTINATION
 # source is a file with one url per line
 # destination is the destination folder the content will be downloaded to
 
-alias bytdl="bulk_youtube_dl"
-
 function bulk_youtube_dl {
-  # Validate parameters
-  #   * presence in function call
-  #   * existence
   if (( $# < 2 )); then
-    print "usage: bulk_youtube_dl SOURCE DESTINATION" >&2
+    echo "usage: bulk_youtube_dl SOURCE DESTINATION" >&2
     return 1
   elif [[ ! -f "$1" ]]; then
-    print "$1 does not exist" >&2
+    echo "$1 does not exist" >&2
     return 1
   elif [[ ! -d "$2" ]]; then
-    echo 'test'
-    print "$2 does not exist" >&2
+    echo "$2 does not exist" >&2
     return 1
   fi
 
-  while read -r line; do
+  while read -r url; do
     youtube-dl --exec "mv {} $2" -x --audio-format mp3 -o "%(title)s.%(ext)s" $url
   done <<< $(cat "$1")
 }
--- spec/bulk_youtube_dl_spec.sh
+++ spec/bulk_youtube_dl_spec.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/env/bin bash
 
 source '../lib/bulk_youtube_dl.sh'
 
@@ -18,24 +18,21 @@ fake youtube-dl _youtube-dl
 
 
 test_returns_an_error_message_if_no_source_is_passed_as_parameter() {
-  actual_message=$(bulk_youtube_dl 2>&1)
   expected_message='usage: bulk_youtube_dl SOURCE DESTINATION'
 
-  assert 'echo "$actual_message" | grep "$expected_message"'
+  assert 'echo "$(bulk_youtube_dl 2>&1)" | grep "$expected_message"'
 }
 
 test_returns_an_error_message_if_the_source_does_not_exists() {
-  actual_message=$(bulk_youtube_dl "${source}_diff" "$destination" 2>&1)
   expected_message="${source}_diff does not exist"
 
-  assert 'echo "$actual_message" | grep "$expected_message"'
+  assert 'echo "$(bulk_youtube_dl "${source}_diff" "$destination" 2>&1)" | grep "$expected_message"'
 }
 
 test_returns_an_error_message_if_the_destination_does_not_exists() {
-  actual_message=$(bulk_youtube_dl "$source" "${destination}_diff" 2>&1)
   expected_message="${destination}_diff does not exist"
 
-  assert 'echo "$actual_message" | grep "$expected_message"'
+  assert 'echo "$(bulk_youtube_dl "$source" "${destination}_diff" 2>&1)" | grep "$expected_message"'
 }
 
 test_download_the_content_and_name_the_file_properly() {
